cef3387dac60673006b8d581be405e8840acac52
AlexOrtiz201 <alexanderortiz.201@gmail.com>
2016-08-29 13:49:56 -0400

    [WIP] Added some comments to explain idea of registerCartridge in the future

--- cartridges/index.js
+++ cartridges/index.js
@@ -4,7 +4,18 @@ import {
 } from '../core/public/utils/cartridges/cartridges-utils';
 import incubatorRoute from './mobimon-incubator/';
 
-registerCartridge(incubatorRoute);
+/**
+ * Registers routes, this should just import
+ * any mobimon-* folders' index.js file.
+ * and return an array the loops through registerCartridge.
+ */
+const allCartridges = [incubatorRoute];
+
+const globFoldersForCartridges = () => allCartridges;
+
+globFoldersForCartridges().forEach((route) => {
+  registerCartridge(route);
+});
 
 const load = () => loadCartridges();
 
